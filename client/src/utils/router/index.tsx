5e0bf62362c530c1798dd3ddbf1ccd2f99d599b2
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-02-03 00:20:34 +0200

    chore: added sign-in route to router

--- client/src/utils/router/index.tsx
+++ client/src/utils/router/index.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout';
 import { Routes, Route } from 'react-router-dom';
+import SignInPage from '@/pages/SignIn';
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
         <Route path="products" element={<>Products</>} />
         <Route path="recipes" element={<>Recipes</>} />
       </Route>
+      <Route path="/auth/sign-in" element={<SignInPage />} />
     </Routes>
   );
 };
