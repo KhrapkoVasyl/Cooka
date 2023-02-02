51f007d39f22d3293e1bc6f8d1859fa361bfc48c
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-02-02 16:26:09 +0200

    refactor: rewrote main to use ProviderWrapper

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ToastContainer } from 'react-toastify';
-import App from './App';
 import './index.css';
-import RootStoreProvider from './stores';
-import { store } from './stores/root.store';
-import { BrowserRouter } from 'react-router-dom';
-import ThemeConfig from './utils/theme';
 import 'typeface-jost';
+import App from '@/App';
+import { ProviderWrapper } from '@/utils/components/ProviderWrapper';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeConfig>
-        <RootStoreProvider store={store}>
-          <App />
-          <ToastContainer position="top-right" />
-        </RootStoreProvider>
-      </ThemeConfig>
-    </BrowserRouter>
-  </React.StrictMode>
+  <ProviderWrapper>
+    <App />
+  </ProviderWrapper>
 );
