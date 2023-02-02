e537aeaa933f011aab40d52745de4747feb0842b
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-02-02 16:25:08 +0200

    refactor: move all app providers to wrapper component

---
+++ client/src/utils/components/ProviderWrapper.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode, StrictMode } from 'react';
+import ThemeConfig from '@/utils/theme';
+import RootStoreProvider from '@/stores';
+import { store } from '@/stores/root.store';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const ProviderWrapper: FC<Props> = ({ children }) => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <ThemeConfig>
+          <RootStoreProvider store={store}>
+            {children}
+            <ToastContainer position="top-right" />
+          </RootStoreProvider>
+        </ThemeConfig>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
