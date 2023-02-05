72979f142471cdef8d124cb63db6bfb7a751b9a3
KhrapkoVasyl <khrapko2002@gmail.com>
2023-02-05 17:13:17 +0200

    feat: add an example of using swagger to set controller tags

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger/dist';
 import { AppService } from './app.service';
 
+@ApiTags('example')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
